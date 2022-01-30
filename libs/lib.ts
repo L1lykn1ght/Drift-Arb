f796e201e6854587259611bd715f7d8cbb202a73
Lilyknight <fortunetamu@gmail.com>
2022-01-30 20:11:33 +0900

    updateNumber追加

--- libs/lib.ts
+++ libs/lib.ts
@@ -1,3 +1,13 @@
 export const sleep = async (ms: number) => {
     return new Promise(r => setTimeout(r, ms))
 }
+
+export const updateNumber = {
+    ftx: {
+        LUNA: 0.005,
+        AVAX: 0.005,
+        MATIC: 0.00001,
+        ATOM: 0.001,
+        DOT: 0.002
+    }
+}
